8f9781f11a6d851108b2a5f2b321dff617e334d0
PaPazhao <lizhao15431230@hotmail.com>
2023-12-16 12:43:11 +0800

    Update 19. 删除链表的倒数第 N 个结点.c

--- src/linked_list/19. 删除链表的倒数第 N 个结点.c
+++ src/linked_list/19. 删除链表的倒数第 N 个结点.c
@@ -1,21 +1,30 @@
 // 19. 删除链表的倒数第 N 个结点
 // 中等
 // https://leetcode.cn/problems/remove-nth-node-from-end-of-list/submissions/407434490/
-struct ListNode *removeNthFromEnd(struct ListNode *head, int n)
-{
-    struct ListNode dummy = {0, head};
-    struct ListNode *fast = &dummy, *slow = &dummy;
-    while (n > 0)
-    {
+struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
+    struct ListNode tmp = {
+        .val = 0, 
+        .next = head
+    };
+
+    struct ListNode *fast = &tmp;
+    struct ListNode *slow = &tmp;
+    
+    while(n) {
         fast = fast->next;
-        n -= 1;
+        n--;
     }
 
-    while (fast && fast->next)
-    {
+    while(fast && fast->next) {
         fast = fast->next;
         slow = slow->next;
     }
-    slow->next = slow->next->next;
-    return dummy.next;
+
+    if(slow->next) {
+        slow->next = slow->next->next;
+    } else {
+        slow->next = NULL;
+    }
+    
+    return tmp.next;
 }
