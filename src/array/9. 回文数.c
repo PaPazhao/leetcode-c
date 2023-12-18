c029f4ce0cab5401d830ed3349d7e3336c6b2d79
PaPazhao <lizhao15431230@hotmail.com>
2023-12-18 17:25:41 +0800

    Update 9. 回文数.c

--- src/array/9. 回文数.c
+++ src/array/9. 回文数.c
@@ -1,21 +1,13 @@
 // https://leetcode.cn/problems/palindrome-number/description/
 bool isPalindrome(int x) {
-    if(x < 0) return false;
+    if(x < 0 || (x % 10 == 0 && x != 0)) return false;
 
-    char bit[100] = {0};
-    char len = 0;
-
-    while(x > 0) {
-        bit[len++] = x % 10;
-        x = x / 10;
+    int revertedNumber  = 0;
+ 
+    while(x > revertedNumber) {
+        revertedNumber  = revertedNumber  * 10 + x % 10;
+        x /= 10;
     }
 
-    char num = 0;
-    while(num < len / 2) {
-        if( bit[num] != bit[len - num - 1] ) {
-            return false;
-        }
-        num++;
-    }
-    return true;
+    return revertedNumber == x || revertedNumber  / 10;
 }
