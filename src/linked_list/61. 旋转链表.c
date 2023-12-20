293491a719407cf6b50263f0854c4a85d0942762
PaPazhao <lizhao15431230@hotmail.com>
2023-12-20 13:26:53 +0800

    Update 61. 旋转链表.c

--- src/linked_list/61. 旋转链表.c
+++ src/linked_list/61. 旋转链表.c
@@ -1,41 +1,37 @@
 // 61. 旋转链表
 // 中等
 // https://leetcode.cn/problems/rotate-list/description/
-
-
-struct ListNode* rotateRight(struct ListNode* head, int k){
-    struct ListNode *p = head, *fast = head, *slow = head, *res = NULL;
-
-    int len = 1;
-    if(head == NULL) {
-        return NULL;
-    }
-
-    while(p->next) {
-        len += 1;
-        p = p->next;
-    }
-
-    int step = k % len;
-
-    if(step == 0) {
+struct ListNode* rotateRight(struct ListNode* head, int k) {
+    if(k == 0 || head == NULL) {
         return head;
     }
 
-
-    while(step) {
-        fast = fast->next;
-        step -= 1;
+    struct ListNode *cur = head;
+    int len = 0;
+    while(cur) {
+        len++;
+        cur = cur->next;
+    }
+    k = k % len;
+    if(k == 0) {
+        return head;
     }
 
-    while(fast->next) {
+    struct ListNode dummy = {0, head};
+    struct ListNode *fast = &dummy, *slow = &dummy;
+    while(k) {
         fast = fast->next;
+        k--;
+    }
+    while(fast && fast->next) {
         slow = slow->next;
+        fast = fast->next;
     }
 
-    p->next = head;
-    res = slow->next;
+    struct ListNode *ret = slow->next;
     slow->next = NULL;
-    return res;
+    fast->next = head;
+    return ret;
+
 }
 
