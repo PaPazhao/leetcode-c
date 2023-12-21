1b7cd4eab83342e85af5602dfdea37a72b30b932
PaPazhao <lizhao15431230@hotmail.com>
2023-12-21 22:08:12 +0800

    Update 704. 二分查找.c

--- src/binary_search/704. 二分查找.c
+++ src/binary_search/704. 二分查找.c
@@ -1,15 +1,18 @@
+// https://leetcode.cn/problems/binary-search/description/
 int search(int* nums, int numsSize, int target) {
     int left = 0, right = numsSize - 1;
 
     while(left <= right) {
         int mid = left + (right - left) / 2;
-        if(nums[mid] > target) {
-            right = mid - 1;
-        } else if(nums[mid] < target) {
-            left = mid + 1;
-        } else if (nums[mid] == target) {
+        int midValue = nums[mid];
+        if(midValue == target) {
             return mid;
+        } else if (midValue > target) {
+            right = mid - 1;
+        } else if (midValue < target) {
+            left = mid + 1;
         }
     }
+
     return -1;
 }
