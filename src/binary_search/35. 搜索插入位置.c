38c740078a4afa1063954eaed057ae8933f3e731
PaPazhao <lizhao15431230@hotmail.com>
2023-12-23 16:49:18 +0800

    Update 35. 搜索插入位置.c

--- src/binary_search/35. 搜索插入位置.c
+++ src/binary_search/35. 搜索插入位置.c
@@ -1,27 +1,20 @@
 // 35. 搜索插入位置
 // 简单
 // https://leetcode.cn/problems/search-insert-position/description/
-int searchInsert(int *nums, int numsSize, int target)
-{
+int searchInsert(int* nums, int numsSize, int target) {
     int left = 0, right = numsSize - 1;
 
-    while (left <= right)
-    {
+    while(left <= right) {
         int mid = left + (right - left) / 2;
-        int value = nums[mid];
-        printf("%d - %d\n", mid, value);
-        if (value == target)
-        {
+        int midValue = nums[mid];
+        if(midValue == target) {
             return mid;
-        }
-        else if (target > value)
-        {
+        } else if(target > midValue) {
             left = mid + 1;
-        }
-        else if (target < value)
-        {
+        } else if(target < midValue) {
             right = mid - 1;
         }
     }
+
     return left;
 }
