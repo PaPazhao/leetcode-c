bb369d1f0d21e16ece6c32256b0c455e116aa8aa
PaPazhao <lizhao15431230@hotmail.com>
2023-12-31 13:37:55 +0800

    Update 191. 位1的个数.c

--- src/191. 位1的个数.c
+++ src/191. 位1的个数.c
@@ -1,23 +1,10 @@
 //https://leetcode.cn/problems/number-of-1-bits/description/
-typedef union Word {
-    uint8_t byte[4];
-    uint16_t halfWorld[2];
-    uint32_t word;
-} Word;
+
 int hammingWeight(uint32_t n) {
-    Word data;
-    data.word = n;
-    int total = 0;
-    for(uint8_t i = 0; i < 4; i++) {
-        uint8_t value = data.byte[i];
-        if(value == 0) {
-            continue;
-        }
-        for(uint8_t j = 0; j < 8; j++) {
-            if(value & (1 << j)) {
-                total++;
-            }
-        }
+    int res = 0;
+    while (n != 0) {
+        n = n & (n - 1);
+        res++;
     }
-    return total;
+    return res; 
 }
