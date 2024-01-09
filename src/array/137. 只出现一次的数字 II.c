192487b99db05e15009a01ba3a48f20f276efcbe
PaPazhao <lizhao15431230@hotmail.com>
2024-01-09 22:12:40 +0800

    Update 137. 只出现一次的数字 II.c

--- src/array/137. 只出现一次的数字 II.c
+++ src/array/137. 只出现一次的数字 II.c
@@ -9,3 +9,17 @@ int singleNumber(int* nums, int numsSize) {
     }
     return b;
 }
+
+int singleNumber(int *nums, int numsSize) {
+    int ans = 0;
+    for (int i = 0; i < 32; ++i) {
+        int total = 0;
+        for (int j = 0; j < numsSize; ++j) {
+            total += ((nums[j] >> i) & 1);
+        }
+        if (total % 3) {
+            ans |= (1u << i);
+        }
+    }
+    return ans;
+} 
