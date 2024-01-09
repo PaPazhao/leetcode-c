9e95087d10bdc14d30c76281810e3eba85b4a774
PaPazhao <lizhao15431230@hotmail.com>
2024-01-09 21:54:17 +0800

    Create  代码 测试用例 测试用例 测试结果 137. 只出现一次的数字 II.c

---
+++ src/array/代码 测试用例 测试用例 测试结果 137. 只出现一次的数字 II.c
@@ -0,0 +1,14 @@
+//https://leetcode.cn/problems/single-number-ii/
+int singleNumber(int* nums, int numsSize) {
+    int ans = 0;
+    for(int i = 0; i < 32; i++) {
+        int total = 0;
+        for(int j = 0; j < numsSize; j++) {
+            total += ((nums[j] >> i) & 1);
+        }
+        if(total % 3) {
+            ans |= (1u << i);
+        }
+    }
+    return ans;
+}
