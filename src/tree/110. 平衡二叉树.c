ea3377d42cdcf4c6faf13211a33f27a275df9424
papazhao <lizhao15431230@qq.com>
2024-01-10 21:50:08 +0800

    110.  平衡二叉树.c

---
+++ src/tree/110. 平衡二叉树.c
@@ -0,0 +1,18 @@
+//https://leetcode.cn/problems/balanced-binary-tree/
+int height(struct TreeNode* root) {
+    if (root == NULL) {
+        return 0;
+    } 
+    int leftHeight = height(root->left);
+    int rightHeight = height(root->right);
+
+    if(leftHeight == -1 || rightHeight == -1 || fabs(leftHeight - rightHeight) > 1) {
+        return -1;
+    }
+
+    return fmax(leftHeight, rightHeight) + 1;
+}
+
+bool isBalanced(struct TreeNode* root) {
+    return height(root) >= 0;
+}
