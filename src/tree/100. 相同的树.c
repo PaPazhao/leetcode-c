907392fa60b741ae57c14eb7c77186f6f800c5bc
papazhao <lizhao15431230@qq.com>
2024-01-10 20:41:29 +0800

    Update 100.相同的树.c

--- src/tree/100. 相同的树.c
+++ src/tree/100. 相同的树.c
@@ -1,16 +1,19 @@
 //https://leetcode.cn/problems/same-tree/
 bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
-    if(NULL == p && NULL == q) {
+    if(p == NULL && q == NULL) {
         return true;
     }
-    if(p && NULL == q) {
-        return false;
-    }
-    if(q && NULL == p) {
+    if((p && q) == NULL) {
         return false;
     }
     if(p->val != q->val) {
         return false;
     }
-    return isSameTree(p->left, q->left) &&  isSameTree(p->right, q->right);
+    if(!isSameTree(p->left, q->left)) {
+        return false;
+    }
+    if(!isSameTree(p->right, q->right)) {
+        return false;
+    }
+    return true;
 }
