a7a38712581064b2a8d28fec138572e7906de79c
PaPazhao <lizhao15431230@hotmail.com>
2024-01-10 20:19:06 +0800

    Update 94. 二叉树的中序遍历.c

--- src/tree/94. 二叉树的中序遍历.c
+++ src/tree/94. 二叉树的中序遍历.c
@@ -1,15 +1,15 @@
-void preorder(struct TreeNode *root, int *res, int*size) {
+void traversal(struct TreeNode *root, int *res, int*size) {
     if(root == NULL) {
         return;
     }
-    preorder(root->left, res, size);
+    traversal(root->left, res, size);
     res[(*size)++] = root->val;
-    preorder(root->right, res, size);
+    traversal(root->right, res, size);
 }
 
 int* inorderTraversal(struct TreeNode* root, int* returnSize) {
-    int *res = malloc(sizeof(int) * 500);
     *returnSize = 0;
-    preorder(root, res, returnSize);
-    return res;
+    int* result = malloc(sizeof(int) * 100);
+    traversal(root, result, returnSize);
+    return result; 
 }
